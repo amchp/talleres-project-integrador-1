76a2af025ee7e19120f2a8e913d71023a6fbe193
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2022-08-02 10:07:10 -0500

    Parte 4

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Alejandro Mc Ewen'})
 
 def about(request):
     return render(request, 'about.html')
